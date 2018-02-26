171776d7ca57e9fdf09f48093f50064ff9e3aaea
Zaki Wasik <zaki@webconsu.lt>
2018-02-26 13:09:53 +0100

    Improve gallery/portfolio.

--- src/components/Portfolio/PortfolioItem.js
+++ src/components/Portfolio/PortfolioItem.js
@@ -3,10 +3,18 @@ import { Link } from 'react-router-dom';
 import Gallery from "../Gallery/Gallery";
 
 class PortfolioItem extends Component {
+
+  spinalCase(str) {
+    return str.replace(/(?!^)([A-Z])/g, ' $1')
+      .replace(/[_\s]+(?=[a-zA-Z])/g, '-').toLowerCase();
+  }
+
   render() {
 
     const {employer, portfolio, label, tags, gallery} = this.props.position;
     const {show, link, icon, description } = portfolio;
+
+    const portfolioCase = this.props.position.case;
 
     if (!show) {
       return null;
@@ -21,15 +29,15 @@ class PortfolioItem extends Component {
     if (label) {
       modifier = 'dark';
       labelElm = <span className="label">{label}</span>;
-      caseBtn = <Link to={'/cases/dr'} className="portfolio-item__case-btn">Show case</Link>;
+      caseBtn = <Link to={portfolioCase.path} className="portfolio-item__case-btn">Show case</Link>;
+    } else {
+      if (gallery) {
+        galleryElm = <Gallery gallery={gallery} hidden={true} id={`gallery-${this.spinalCase(employer)}`}/>
+      }
     }
 
     if (link) {
       linkElm = <div className="employer-link"><a href={link}>Visit {employer}'s website</a></div>
-    }
-
-    if (gallery) {
-      galleryElm = <Gallery gallery={gallery}/>
     }
 
 
@@ -45,6 +53,7 @@ class PortfolioItem extends Component {
           <h2 className="portfolio-item__employer">{employer}</h2>
           <p>{description}</p>
           {caseBtn}
+          {galleryElm}
           {linkElm}
           <div className="portfolio-item__tag-container">
             <h4 className="portfolio-item__tag-heading">Tags:</h4>
@@ -54,8 +63,8 @@ class PortfolioItem extends Component {
               ))}
             </ul>
           </div>
-          {galleryElm}
         </div>
+
       </div>
     );
   }
--- src/components/gallery/Gallery.js
+++ src/components/gallery/Gallery.js
@@ -8,7 +8,17 @@ const elementsPerRow = 3;
 class Gallery extends Component {
   componentDidMount() {
     // Instantiate a "lightbox Gallery"
-    baguetteBox.run('.gallery');
+    const galleries = baguetteBox.run('.gallery');
+
+    const galleryButtons = document.querySelectorAll('.gallery__button');
+
+    for (let i = 0; i < galleryButtons.length; i++) {
+      galleryButtons[i].addEventListener('click', (e)=> {
+        e.preventDefault();
+
+        baguetteBox.show(0, galleries[i]);
+      });
+    }
   }
 
   // Layout logic. One item: full-width, two-items: 1/2 grid, < 3 items: 1/3 grid
@@ -25,17 +35,27 @@ class Gallery extends Component {
     }
   }
 
+
   render() {
-    const { gallery } = this.props;
+
+    const { gallery, hidden, id } = this.props;
+    let galleryBtn;
+
+    if (hidden) {
+      galleryBtn = <a href={`#${id}`} className="gallery__button">Screenshots</a>
+    }
 
     return (
-      <div className="gallery">
-        <h3 className="gallery__heading">Screenshots:</h3>
-        <ul>
-          {gallery.map((picture, i) => (
-            <GalleryItem key={i} picture={picture} layout={this.getLayout(gallery.length)}/>
-          ))}
-        </ul>
+      <div className="gallery" id={id}>
+        {galleryBtn}
+        <div className={`gallery-container${hidden ? '--hidden' : '' }`}>
+          <h3 className="gallery__heading">Screenshots:</h3>
+          <ul>
+            {gallery.map((picture, i) => (
+              <GalleryItem key={i} picture={picture} layout={this.getLayout(gallery.length)}/>
+            ))}
+          </ul>
+        </div>
       </div>
     );
   }
